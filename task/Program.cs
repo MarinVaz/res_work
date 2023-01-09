3ff99fd9fe39239e0e6a76ee23511744ad05295f
Marina <errorda@gmail.com>
2023-01-09 15:14:54 +0300

    added functions

--- task/Program.cs
+++ task/Program.cs
@@ -1,7 +1,38 @@
 ﻿string[] array = WriteArray();
+string[] result = FindLessThan(array, 3);
 
 string[] WriteArray()
 {
    Console.Write("Введите значения через пробел: ");
     return Console.ReadLine().Split(" ");
+}
+
+string[] FindLessThan(string[] input, int n) 
+{
+   string[] output = new string[CountLessThan(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) 
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int CountLessThan(string[] input, int n) 
+{
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
 }
